3375323695549d9802b9bfa5df5085bdc5d0ecdc
chenmeng <chenmeng_1919@163.com>
2022-04-21 14:45:11 +0800

    增加手工收入分配的路由

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,41 @@ import notfound from "../components/notfound";
 import lobtrace from "../components/lobtrace";
 import monthlycheckout from "../components/monthlycheckout";
 import querydata from "../components/querydata";
+import addDetails from "../components/manualincomedis/addDetails";
+import addIncomedisHead from "../components/manualincomedis/addIncomedisHead";
+import incomedisDetails from "../components/manualincomedis/incomedisDetails";
+import incomedisHead from "../components/manualincomedis/incomedisHead";
+import uploadFile from "../components/manualincomedis/uploadFile";
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/uploadFile',
+      name: "uploadFile",
+      component: uploadFile
+    },
+    {
+      path: '/incomedisHead',
+      name: "incomedisHead",
+      component: incomedisHead
+    },
+    {
+      path: '/incomedisDetails',
+      name: "incomedisDetails",
+      component: incomedisDetails
+    },
+    {
+      path: '/addIncomedisHead',
+      name: "addIncomedisHead",
+      component: addIncomedisHead
+    },
+    {
+      path: '/addDetails',
+      name: "addDetails",
+      component: addDetails
+    },
     {
       path: '/querydata',
       name: "querydata",
